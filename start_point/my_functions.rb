2f8af104393fd3e419c69248f13d2f822dc75737
Richard <fahitch+github@gmail.com>
2019-10-16 15:26:26 +0100

    second function defined

--- start_point/my_functions.rb
+++ start_point/my_functions.rb
@@ -1,3 +1,11 @@
 def add_array_lengths( array1, array2)
   return array1.length + array2.length
 end
+
+def sum_array(numbers)
+  counter = 0
+  for number in numbers
+    counter += number
+  end
+  return counter
+end
